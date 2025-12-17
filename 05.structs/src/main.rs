c6e8c2e12e24bf1e5da5456233101235d87c08ab
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2025-12-18 00:41:34 +0700

    struct

---
+++ 05.structs/src/main.rs
@@ -0,0 +1,61 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("username1"),
+        email: String::from("email1@gmail.com"),
+        sign_in_count: 1,
+    };
+
+    println!("{} {} {} {}", user1.username, user1.active, user1.email, user1.sign_in_count);
+    // user1.email = String::from("email11@gmail.com"); Cannot assign value to immutable user1
+    
+    let mut user2 = User{
+        active: false,
+        username: String::from("username2"),
+        email: String::from("email2@gmail.com"),
+        sign_in_count: 0,
+    };
+    println!("{} {} {} {}", user2.username, user2.active, user2.email, user2.sign_in_count);
+    user2.email = String::from("email2222@gmail.com");
+    println!("{} {} {} {}", user2.username, user2.active, user2.email, user2.sign_in_count);
+
+    let mut user3 = build_user(String::from("email3@gmail.com"), String::from("username3"));
+    user3.email = String::from("email333@gmail.com");
+    println!("{} {} {} {}", user3.username, user3.active, user3.email, user3.sign_in_count);
+
+    let user4 = User {
+        username: String::from("username4"),
+        email: String::from("email4@gmail.com"),
+        ..user3
+    };
+    println!("{} {} {} {}", user4.username, user4.active, user4.email, user4.sign_in_count);
+
+    // user3 still can be accessed
+    // because active and sign_in_count are the types that implement Copy trait
+    println!("{} {} {} {}", user3.username, user3.active, user3.email, user3.sign_in_count);
+
+    let user5 = User {
+        username: String::from("username5"),
+        ..user3
+    };
+
+    // user3 cannot be used 
+    // because user, email are the types that implement Move trait
+    // println!("{} {} {} {}", user3.username, user3.active, user3.email, user3.sign_in_count);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
