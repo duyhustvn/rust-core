28ea4fc317aaa047f6df149c06f167f7e22c124d
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2025-12-29 00:09:08 +0700

    string

--- 08.collection/src/str.rs
+++ 08.collection/src/str.rs
@@ -2,29 +2,29 @@ pub fn str() {
     let data = "initial contents";
 
     let s = data.to_string(); // convert str to String
-    println!("s: {}", s);
+    println!("s: {s}");
 
     // the method also works on a literal directly
     let s = "initial contents".to_string();
-    println!("s: {}", s);
+    println!("s: {s}");
 
     let s = String::from("initial contents");
-    println!("s: {}", s);
+    println!("s: {s}");
     // s.push_str("hello"); // cannot update immutable string
 
     let mut s = String::from("foo");
     s.push_str("bar");
-    println!("s: {}", s);
+    println!("s: {s}");
 
     let mut s1 = String::from("foo");
     let s2 = "bar";
     s1.push_str(s2); // push_str take reference of string not ownership
-    println!("s2: {}", s2);
-    println!("s1: {}", s1);
+    println!("s2: {s2}");
+    println!("s1: {s1}");
 
     let mut s = String::from("lo");
     s.push('l');
-    println!("s: {}", s);
+    println!("s: {s}");
 
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
@@ -33,11 +33,22 @@ pub fn str() {
     // let s3 = s1 + s2;
     // let s3 = &s1 + s2;
     // let s3 = &s1 + &s2;
-    println!("s3: {}", s3);
+    println!("s3: {s3}");
 
     let s1 = String::from("tic");
     let s2 = String::from("tac");
     let s3 = String::from("toe");
     let s = format!("{s1}-{s2}-{s3}");
-    println!("s: {}", s);
+    println!("s: {s}");
+
+    let s = String::from("Зд");
+    for c in s.chars() {
+        print!("{c} ");
+    }
+    println!("");
+
+    for b in s.bytes() {
+        print!("{b} ");
+    }
+    println!("");
 }
