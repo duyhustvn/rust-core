ef5e116e526924cd35d91c38758eaaa047358ab1
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-01-08 07:11:30 +0700

    minigrep

--- 12.minigrep/src/main.rs
+++ 12.minigrep/src/main.rs
@@ -1,20 +1,41 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    if args.len() != 3 {
-        panic!("Syntax: cargo run -- [search_string] [file]");
-    }
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Search for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Search for {query}");
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Syntax: cargo run -- [search_string] [file]");
+        }
+
+        let query = &args[1];
+        let file_path = &args[2];
+
+        Ok(Config {
+            query: String::from(query),
+            file_path: String::from(file_path),
+        })
+    }
 }
