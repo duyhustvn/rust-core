8ed7df549d443143feb2f579a0952b1721cad8aa
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-08-06 12:41:52 +0700

    concurrency

---
+++ 16.concurrency/src/main.rs
@@ -0,0 +1,15 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+}
